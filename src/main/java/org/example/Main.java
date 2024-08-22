20f803ca8cf6c7f1ad77877566f65e30b634834e
KavinduLakshanFernando <kavindu2003fernando@gmail.com>
2024-08-22 20:58:11 +0530

    Done

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import org.example.config.FactoryConfiguration;
+import org.example.embed.FullName;
+import org.example.entity.Student;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+public class Main {
+    public static void main(String[] args) {
+        Session session= FactoryConfiguration.getInstance().getSession();
+
+        FullName fullName = new FullName("Ashan","Malshan");
+
+        Student student = new Student();
+        student.setId(3);
+        student.setName(fullName);
+        student.setAddress("panadura");
+        Transaction transaction = session.beginTransaction();
+
+        //delete,get another Table,note
+        session.save(student);
+      // session.delete(student);
+
+        Student student1 = session.get(Student.class,1);
+        if (student1 != null){
+            System.out.println(student1.getId());
+            System.out.println(student1.getAddress());
+            System.out.println(student1.getName().getFirstName());
+            System.out.println(student1.getName().getLastName());
+        }
+        transaction.commit();
+        session.close();
+    }
+}
